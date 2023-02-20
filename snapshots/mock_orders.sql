c2aba264519ac8b8d3d63264cfc8b0ded330d789
atulshahu <63558964+atulshahu@users.noreply.github.com>
2023-02-21 00:44:54 +0530

    Add files via upload

---
+++ snapshots/mock_orders.sql
@@ -0,0 +1,19 @@
+{% snapshot mock_orders %}
+
+{% set new_schema = target.schema + '_snapshot' %}
+
+{{
+    config(
+      target_database='dbt_practice',
+      target_schema=new_schema,
+      unique_key='order_id',
+
+      strategy='timestamp',
+      updated_at='updated_at',
+    )
+}}
+
+select * from dbt_practice.{{target.schema}}.mock_orders
+
+{% endsnapshot %}
+	
